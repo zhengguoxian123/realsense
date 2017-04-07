a556456e11fa87f0bb57d96a767fd89a4f2160e6
Helen Oleynikova <helenoleynikova@gmail.com>
2017-04-07 10:16:10 +0200

    Add time sync from our private driver in.

---
+++ realsense_camera/include/realsense_camera/time_sync.h
@@ -0,0 +1,49 @@
+/*
+ * time_sync.h
+ *
+ *  Created on: Sep 21, 2016
+ *      Authors: Michael Burri, Helen Oleynikova
+ */
+
+#include <iostream>
+
+#include <Eigen/Dense>
+#include <ros/ros.h>
+
+#ifndef REALSENSE_CAMERA_TIME_SYNC_H_
+#define REALSENSE_CAMERA_TIME_SYNC_H_
+
+namespace realsense_ros {
+
+class TimeSyncFilter {
+ public:
+  static constexpr double kSigmaInitOffset = 2e-3;
+  static constexpr double kSigmaInitSkew = 1e-3;
+  static constexpr double kSigmaMeasurementTimeOffset = 2e-3;
+  static constexpr double kSigmaSkew = 2e-6;
+  static constexpr double kSigmaOffset = 1e-5;
+  static constexpr double kUpdateRate = 0.1; // (1/sec)
+
+  TimeSyncFilter();
+  ~TimeSyncFilter() {};
+  double getLocalTimestamp(double device_time);
+  void updateFilter(double device_time, double local_time);
+  void print();
+  void reset();
+ private:
+  void initialize(double device_time, double local_time);
+  bool isOutlier(double device_time, double local_time);
+
+  Eigen::Vector2d x_;
+  Eigen::Matrix2d P_;
+  Eigen::Matrix2d Q_;
+  double R_;
+  Eigen::Matrix<double, 1, 2> H_;
+  bool is_initialized_;
+  double last_update_device_time_;
+  double dt_;
+};
+
+}  // realsense ros
+
+#endif  // REALSENSE_CAMERA_TIME_SYNC_H_
